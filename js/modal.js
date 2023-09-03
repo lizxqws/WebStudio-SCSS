a902a0df48c4859ad19460f7595973b58221ca02
lizxqws <goncharov4ento@gmail.com>
2023-09-04 00:10:00 +0200

    I uploaded the part of the project WebStudio

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+(() => {
+  console.log("start");
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    console.log("start");
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
